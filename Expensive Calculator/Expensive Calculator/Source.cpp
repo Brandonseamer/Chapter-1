451a1854fee91ee910cd183d73adb68274f41708
Brandon Seamer <brandonseamer@gmail.com>
2018-03-27 13:06:07 -0500

    Update

---
+++ Expensive Calculator/Expensive Calculator/Source.cpp
@@ -0,0 +1,22 @@
+//Expensive Calculator
+//3/27/18
+//Author: Brandon Seamer
+
+
+#include <iostream>
+int main()
+{
+	std::cout << "seven + three = " << 7 + 3 << std::endl;
+	std::cout << "7 - 3 = " << 7 - 3 << std::endl;
+	std::cout << "7 X 3 = " << 7 * 3 << std::endl;
+	std::cout << "7.0 / 3.0 = " << 7.0 / 3.0 << std::endl;
+	std::cout << "7 % 3 = " << 7 % 3 << std::endl;
+
+
+	std::cout << "7 + 3 * 5 = " << 7 + 3 * 5 << std::endl;
+	std::cout << "(7+3) * 5 = " << (7 + 3) * 5 << std::endl;
+	system("pause");
+	return 0;
+
+
+}
